b759ac9da44e78c1ba84dba3b697b310b62f6aa8
Raul Chedrese <raulchedrese@gmail.com>
2023-09-25 18:54:31 -0600

    Fix dark mode issue with textarea.

--- mix.exs
+++ mix.exs
@@ -23,13 +23,15 @@ defmodule Satsuma.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix_live_view, "~> 0.20"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 
   defp package() do
     [
+      files: ~w(lib priv .formatter.exs mix.exs README*
+      package.json),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/raulchedrese/satsuma"}
     ]
